d7c0d24765daa10db729b9a7bb7b5b40ff02ba7b
liu946 <liuyang570926881@gmail.com>
2015-09-20 13:52:53 +0800

    field init

---
+++ conf/field.js
@@ -0,0 +1,419 @@
+var field = [
+    {
+        "classname": "乡镇数据",
+        "childfield": [
+            {
+                "fieldname": "镇编号",
+                "field": "XiangZhenShuJuZhenBianHao"
+            },
+            {
+                "fieldname": "镇名称",
+                "field": "XiangZhenShuJuZhenMingChen"
+            },
+            {
+                "fieldname": "镇域人口",
+                "unit": "人",
+                "field": "XiangZhenShuJuZhenYuRenKou"
+            },
+            {
+                "fieldname": "林地面积",
+                "unit": "公顷",
+                "field": "XiangZhenShuJuLinDiMianJi"
+            },
+            {
+                "fieldname": "草地面积",
+                "unit": "公顷",
+                "field": "XiangZhenShuJuCaoDiMianJi"
+            },
+            {
+                "fieldname": "水域面积",
+                "unit": "公顷",
+                "field": "XiangZhenShuJuShuiYuMianJi"
+            },
+            {
+                "fieldname": "耕地面积",
+                "unit": "公顷",
+                "field": "XiangZhenShuJuGengDiMianJi"
+            },
+            {
+                "fieldname": "交通用地面积",
+                "unit": "公顷",
+                "field": "XiangZhenShuJuJiaoTongYongDiMianJi"
+            },
+            {
+                "fieldname": "建设用地面积",
+                "unit": "公顷",
+                "field": "XiangZhenShuJuJianSheYongDiMianJi"
+            },
+            {
+                "fieldname": "镇域土地总面积",
+                "unit": "公顷",
+                "field": "XiangZhenShuJuZhenYuTuDiZongMianJi"
+            },
+            {
+                "fieldname": "农用化肥施用量",
+                "unit": "吨",
+                "field": "XiangZhenShuJuNongYongHuaFeiShiYongLiang"
+            },
+            {
+                "fieldname": "居民人均收入",
+                "unit": "元",
+                "field": "XiangZhenShuJuJiMinRenJunShouRu"
+            },
+            {
+                "fieldname": "玉米种植面积",
+                "unit": "亩",
+                "field": "XiangZhenShuJuYuMiZhongZhiMianJi"
+            },
+            {
+                "fieldname": "玉米产量",
+                "unit": "吨",
+                "field": "XiangZhenShuJuYuMiChanLiang"
+            },
+            {
+                "fieldname": "水稻种植面积",
+                "unit": "亩",
+                "field": "XiangZhenShuJuShuiDaoZhongZhiMianJi"
+            },
+            {
+                "fieldname": "水稻产量",
+                "unit": "吨",
+                "field": "XiangZhenShuJuShuiDaoChanLiang"
+            },
+            {
+                "fieldname": "一产GDP",
+                "unit": "万元",
+                "field": "XiangZhenShuJuYiChanGDP"
+            },
+            {
+                "fieldname": "二产GDP",
+                "unit": "万元",
+                "field": "XiangZhenShuJuErChanGDP"
+            },
+            {
+                "fieldname": "三产GDP",
+                "unit": "万元",
+                "field": "XiangZhenShuJuSanChanGDP"
+            },
+            {
+                "fieldname": "一产从业人员",
+                "unit": "人",
+                "field": "XiangZhenShuJuYiChanCongYeRenYuan"
+            },
+            {
+                "fieldname": "二产从业人员",
+                "unit": "人",
+                "field": "XiangZhenShuJuErChanCongYeRenYuan"
+            },
+            {
+                "fieldname": "三产从业人员",
+                "unit": "人",
+                "field": "XiangZhenShuJuSanChanCongYeRenYuan"
+            },
+            {
+                "fieldname": "生活污水处理率",
+                "unit": "%",
+                "field": "XiangZhenShuJuShengHuoWuShuiChuLiLv"
+            },
+            {
+                "fieldname": "调查问卷总户数",
+                "unit": "户",
+                "field": "XiangZhenShuJuDiaoChaWenJuanZongHuShu"
+            },
+            {
+                "fieldname": "镇域道路总长度",
+                "unit": "公里",
+                "field": "XiangZhenShuJuZhenYuDaoLuZongChangDu"
+            },
+            {
+                "fieldname": "镇域道路硬化长度",
+                "unit": "公里",
+                "field": "XiangZhenShuJuZhenYuDaoLuYingHuaChangDu"
+            },
+            {
+                "fieldname": "中学学生人数",
+                "unit": "人",
+                "field": "XiangZhenShuJuZhongXueXueShengRenShu"
+            },
+            {
+                "fieldname": "小学学生人数",
+                "unit": "人",
+                "field": "XiangZhenShuJuXiaoXueXueShengRenShu"
+            },
+            {
+                "fieldname": "学生人数总数",
+                "unit": "人",
+                "field": "XiangZhenShuJuXueShengRenShuZongShu"
+            },
+            {
+                "fieldname": "卫生设施千人病床数",
+                "unit": "个",
+                "field": "XiangZhenShuJuWeiShengSheShiQianRenBingChuangShu"
+            },
+            {
+                "fieldname": "年均降雨量",
+                "unit": "毫米",
+                "field": "XiangZhenShuJuNianJunJiangYuLiang"
+            },
+            {
+                "fieldname": "最高海拔",
+                "unit": "米",
+                "field": "XiangZhenShuJuZuiGaoHaiBa"
+            },
+            {
+                "fieldname": "最低海拔",
+                "unit": "米",
+                "field": "XiangZhenShuJuZuiDiHaiBa"
+            },
+            {
+                "fieldname": "灾害类型数",
+                "unit": "个",
+                "field": "XiangZhenShuJuZaiHaiLeiXingShu"
+            },
+            {
+                "fieldname": "年均降水量",
+                "unit": "毫米",
+                "field": "XiangZhenShuJuNianJunJiangShuiLiang"
+            },
+            {
+                "fieldname": "年均无霜期",
+                "unit": "天",
+                "field": "XiangZhenShuJuNianJunWuShuangQi"
+            },
+            {
+                "fieldname": "本地木本植物种类数",
+                "unit": "个",
+                "field": "XiangZhenShuJuBenDiMuBenZhiWuZhongLeiShu"
+            },
+            {
+                "fieldname": "总木本植物种类数",
+                "unit": "个",
+                "field": "XiangZhenShuJuZongMuBenZhiWuZhongLeiShu"
+            },
+            {
+                "fieldname": "绿色无公害农产品种植面积",
+                "unit": "亩",
+                "field": "XiangZhenShuJuLvSeWuGongHaiNongChanPinZhongZhiMianJi"
+            },
+            {
+                "fieldname": "村镇中学学校建筑总面积",
+                "unit": "平方米",
+                "field": "XiangZhenShuJuCunZhenZhongXueXueXiaoJianZhuZongMianJi"
+            },
+            {
+                "fieldname": "村镇中学学校建筑总面积",
+                "unit": "平方米",
+                "field": "XiangZhenShuJuCunZhenZhongXueXueXiaoJianZhuZongMianJi"
+            },
+            {
+                "fieldname": "村镇卫生设施建筑总面积",
+                "unit": "平方米",
+                "field": "XiangZhenShuJuCunZhenWeiShengSheShiJianZhuZongMianJi"
+            },
+            {
+                "fieldname": "绿色出行距离内学生人数",
+                "unit": "人",
+                "field": "XiangZhenShuJuLvSeChuXingJuLiNaXueShengRenShu"
+            },
+            {
+                "fieldname": "近五年灾害发生频率",
+                "unit": "次每年",
+                "field": "XiangZhenShuJuJinWuNianZaiHaiFaShengPinLv"
+            },
+            {
+                "fieldname": "自然保护区用地面积",
+                "unit": "公顷",
+                "field": "XiangZhenShuJuZiRanBaoHuQuYongDiMianJi"
+            }
+        ],
+        "class": "XiangZhenShuJu"
+    },
+    {
+        "classname": "镇区数据",
+        "childfield": [
+            {
+                "fieldname": "镇区编号",
+                "field": "ZhenQuShuJuZhenQuBianHao"
+            },
+            {
+                "fieldname": "镇区名称",
+                "field": "ZhenQuShuJuZhenQuMingChen"
+            },
+            {
+                "fieldname": "镇区人口",
+                "unit": "人",
+                "field": "ZhenQuShuJuZhenQuRenKou"
+            },
+            {
+                "fieldname": "镇区绿化覆盖率",
+                "unit": "%",
+                "field": "ZhenQuShuJuZhenQuLvHuaFuGaiLv"
+            },
+            {
+                "fieldname": "镇区居民人均收入",
+                "unit": "元",
+                "field": "ZhenQuShuJuZhenQuJiMinRenJunShouRu"
+            },
+            {
+                "fieldname": "镇区文体设施建筑总面积",
+                "unit": "平方米",
+                "field": "ZhenQuShuJuZhenQuWenTiSheShiJianZhuZongMianJi"
+            },
+            {
+                "fieldname": "镇区商业用地总面积",
+                "unit": "公顷",
+                "field": "ZhenQuShuJuZhenQuShangYeYongDiZongMianJi"
+            },
+            {
+                "fieldname": "镇区建设用地总面积",
+                "unit": "公顷",
+                "field": "ZhenQuShuJuZhenQuJianSheYongDiZongMianJi"
+            }
+        ],
+        "class": "ZhenQuShuJu"
+    },
+    {
+        "classname": "村庄数据",
+        "childfield": [
+            {
+                "fieldname": "村庄编号",
+                "field": "CunZhuangShuJuCunZhuangBianHao"
+            },
+            {
+                "fieldname": "村庄名称",
+                "field": "CunZhuangShuJuCunZhuangMingChen"
+            },
+            {
+                "fieldname": "村庄人口",
+                "unit": "人",
+                "field": "CunZhuangShuJuCunZhuangRenKou"
+            },
+            {
+                "fieldname": "村庄居民人均收入",
+                "unit": "元",
+                "field": "CunZhuangShuJuCunZhuangJiMinRenJunShouRu"
+            },
+            {
+                "fieldname": "村庄建设用地总面积",
+                "unit": "公顷",
+                "field": "CunZhuangShuJuCunZhuangJianSheYongDiZongMianJi"
+            }
+        ],
+        "class": "CunZhuangShuJu"
+    },
+    {
+        "classname": "住户数据",
+        "childfield": [
+            {
+                "fieldname": "问卷编号",
+                "field": "ZhuHuShuJuWenJuanBianHao"
+            },
+            {
+                "fieldname": "所属村庄或镇区",
+                "field": "ZhuHuShuJuSuoShuCunZhuangHuoZhenQu"
+            },
+            {
+                "fieldname": "居民对地区环境建设满意度",
+                "field": "ZhuHuShuJuJiMinDuiDiQuHuanJingJianSheManYiDu"
+            },
+            {
+                "fieldname": "居民对地区经济发展满意度",
+                "field": "ZhuHuShuJuJiMinDuiDiQuJingJiFaZhanManYiDu"
+            },
+            {
+                "fieldname": "居民对地区就业情况满意度",
+                "field": "ZhuHuShuJuJiMinDuiDiQuJiuYeQingKuangManYiDu"
+            },
+            {
+                "fieldname": "生活用水满意度",
+                "field": "ZhuHuShuJuShengHuoYongShuiManYiDu"
+            },
+            {
+                "fieldname": "公共设施服务满意度",
+                "field": "ZhuHuShuJuGongGongSheShiFuWuManYiDu"
+            },
+            {
+                "fieldname": "居民对村镇水、电、垃圾基础设施满意度",
+                "field": "ZhuHuShuJuJiMinDuiCunZhenShuiDianLaJiJiChuSheShiManYiDu"
+            },
+            {
+                "fieldname": "居民对道路交通设施满意度",
+                "field": "ZhuHuShuJuJiMinDuiDaoLuJiaoTongSheShiManYiDu"
+            },
+            {
+                "fieldname": "可支配年收入",
+                "unit": "元",
+                "field": "ZhuHuShuJuKeZhiPeiNianShouRu"
+            },
+            {
+                "fieldname": "宅基地面积",
+                "unit": "平方米",
+                "field": "ZhuHuShuJuZhaiJiDiMianJi"
+            },
+            {
+                "fieldname": "农药化肥剩余量",
+                "unit": "千克",
+                "field": "ZhuHuShuJuNongYaoHuaFeiShengYuLiang"
+            },
+            {
+                "fieldname": "耕地规模",
+                "unit": "亩",
+                "field": "ZhuHuShuJuGengDiGuiMo"
+            },
+            {
+                "fieldname": "农田劳作出行距离",
+                "unit": "米",
+                "field": "ZhuHuShuJuNongTianLaoZuoChuXingJuLi"
+            },
+            {
+                "fieldname": "年煤炭消耗",
+                "unit": "千克",
+                "field": "ZhuHuShuJuNianMeiTanXiaoHao"
+            },
+            {
+                "fieldname": "木柴、秸秆消耗",
+                "unit": "千克",
+                "field": "ZhuHuShuJuMuChaiJieGanXiaoHao"
+            },
+            {
+                "fieldname": "剩余农药化肥综合利用量",
+                "unit": "千克",
+                "field": "ZhuHuShuJuShengYuNongYaoHuaFeiZongHeLiYongLiang"
+            },
+            {
+                "fieldname": "是否直接倾倒污水",
+                "field": "ZhuHuShuJuShiFuZhiJieQingDaoWuShui"
+            },
+            {
+                "fieldname": "长距离出行是否绿色",
+                "field": "ZhuHuShuJuChangJuLiChuXingShiFuLvSe"
+            },
+            {
+                "fieldname": "是否使用自来水",
+                "field": "ZhuHuShuJuShiFuShiYongZiLaiShui"
+            },
+            {
+                "fieldname": "户月均用电量",
+                "unit": "度",
+                "field": "ZhuHuShuJuHuYueJunYongDianLiang"
+            },
+            {
+                "fieldname": "是否存在水土流失",
+                "field": "ZhuHuShuJuShiFuCunZaiShuiTuLiuShi"
+            },
+            {
+                "fieldname": "日均短距离绿色出行次数",
+                "unit": "次",
+                "field": "ZhuHuShuJuRiJunDuanJuLiLvSeChuXingCiShu"
+            },
+            {
+                "fieldname": "日均短距离出行次数",
+                "unit": "次",
+                "field": "ZhuHuShuJuRiJunDuanJuLiChuXingCiShu"
+            }
+        ],
+        "class": "ZhuHuShuJu"
+    }
+]
+
+module.exports = field;
