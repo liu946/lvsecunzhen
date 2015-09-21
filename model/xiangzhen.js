63020b8765cd68fd94c39237dfe909c29958deb3
liu946 <liuyang570926881@gmail.com>
2015-09-21 22:11:14 +0800

    model added

---
+++ model/xiangzhen.js
@@ -0,0 +1,10 @@
+
+var model = require("./model.js")
+var sqlhelper = require('../func/sql.js')
+var sys = require("../func/sys.js")
+var datatype = 
+var xiangzhen = sys.clone(model);
+
+xiangzhen.creattable = function () {
+
+}
---
+++ model/zhenquhuocunzhuang.js
@@ -0,0 +1,3 @@
+var model = require("./model.js")
+var sqlhelper = require('../func/sql.js')
+var sys = require("../func/sys.js")
