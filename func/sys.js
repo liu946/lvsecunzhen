802ff84e83f71f1612aaaceca87a69ffdc7fada9
liu946 <liuyang570926881@gmail.com>
2015-09-22 23:34:12 +0800

    model in constructure

--- func/sys.js
+++ func/sys.js
@@ -6,12 +6,21 @@
  */
 
 
-var sys = {
-	clone:function(obj) {
-		var _f = function() {};
+
+
+ var sys = {
+ 	clone:function(obj) {
+ 		var _f = function() {};
 		//这句是原型式继承最核心的地方，函数的原型对象为对象字面量
 		_f.prototype = obj;
 		return new _f;
+	},
+	M:function(modelname){
+		if (typeof(modelname) == "undefined") {
+			modelname = 'model';
+		};
+		var model = require('../model/'+modelname+'.js');
+		return model;
 	}
 }
 
