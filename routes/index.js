01f7edc91e97adc55c6fbf9dd5c47f0a2a6b937b
liu946 <liuyang570926881@gmail.com>
2015-09-30 09:29:38 +0800

    title

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: '绿色村镇体系数据录入系统' });
 });
 
 module.exports = router;
